2837a4a61039c6cfa154ec337b81a96f44c48062
sam <samcesario1@gmail.com>
2017-05-08 00:06:54 -0400

    Server is not compiling

--- hardware/src/v1.0/server/server_v1.c
+++ hardware/src/v1.0/server/server_v1.c
@@ -13,7 +13,7 @@
 #include "./mongoDB_c/mongodb_api.c"
 #include "./socket_conn/socket_conn.c"
 #include "./packets/packet_parser.c"
-
+#include <syslog.h>
 
 int main(int argc, char **argv[])
 {
@@ -43,6 +43,7 @@ int main(int argc, char **argv[])
                 if(bufferIn[0] == 'e'){
                        printPacket(bufferIn);
                        write(sockFd,"eeeeeeeeeeeeeeeeee",buffSize);
+                       syslog(LOG_INFO,"Wrote eee...!");
                 }
                 // Insert Command Received
                 if(bufferIn[14] == 'i'){ //0 right now for clarity 14
